97811782578af860c2fea8328bc819379e743cce
erikpjverweij <erikpjverweij@gmail.com>
2026-05-17 14:43:51 +0200

    Kaarten module 09 en 12 toegevoegd, cache v4, totaal 263 kaarten

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  *     offline maar zie je wel direct nieuwe kaarten zodra je weer bereik hebt.
  */
 
-const CACHE_NAME = 'grotius-overhoor-v3';
+const CACHE_NAME = 'grotius-overhoor-v4';
 const STATIC_ASSETS = [
   './',
   './index.html',
